28770f579b153fd4a6056d7998acd95af1bb6e66
jhonreina <jonathang2812@gmail.com>
2022-10-20 13:35:17 -0500

    connection a base de datos

--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
+const connectDatabase = require("./config/database");
 
 //Setear archivo de configuración
 const dotenv = require("dotenv");
 dotenv.config({ path: 'back/config/config.env'});
+
+// configuracion base de datos
+connectDatabase();
 
 const server = app.listen(process.env.PORT, () => {
   console.log(
