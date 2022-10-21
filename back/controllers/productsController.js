66340c9de999ba99f2a32600d468a6e0bc0f6290
jhonreina <jonathang2812@gmail.com>
2022-10-20 20:09:57 -0500

    CRUD
    Update

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,12 +1,51 @@
+const productos = require("../models/productos");
 const producto = require("../models/productos");
 // ver la lista de productos
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+    const productos = await producto.find();
     res.status(200).json({
         success: true,
-        message:"En esta ruta ud va poder ver todos los productos"
+        cantidad: productos.length,
+        productos
     })
 }
 
+// ver producto por ID
+exports.getProductById = async (req, res, next) => {
+    const product = await producto.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+               success: false,
+               message:"No encontramos el producto"
+        })
+    }
+     res.status(200).json({
+       success: true,
+       message:"Aqui debajo encuentras informacion sobre tu producto:",
+       product
+     });
+}
+// update un produsto
+exports.updateProduct = async (req, res, next) => {
+    let product = await producto.findById(req.params.id);//variable de tipo modificable
+    if (!product) { //verifica que el objeto no existe
+      return res.status(404).json({
+        success: false,
+        message: "No encontramos el producto",
+      });
+    }
+    // si el objeto si existe ejecuto la actulizacion
+    product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, //valida solo los atributos nuevos o actualizados
+        runValidators: true,        
+    });
+    // responde ok si el objeto se actualizo
+    res.status(200).json({
+        success: true,
+        massage: "Producto actualizado correctamente",
+        product
+    })
+}
 // crear nuevo producto /api/prodcutos
 exports.newProduct = async (req, res, next) => {
     const product = await producto.create(req.body);
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, newProduct } = require("../controllers/productsController");
+const { getProducts, newProduct, getProductById, updateProduct } = require("../controllers/productsController");
 
 //traemos la respuesta json desde el controlador
 //establecemos desde que ruta queremos ver el getproducts
 router.route('/productos').get(getProducts)
-router.route('/producto/nuevo').post(newProduct)
+router.route('/producto/nuevo').post(newProduct)//Establecemos ruta
+router.route('/producto/:id').get(getProductById);//ruta para consultar ID 
+router.route('/producto/:id').put(updateProduct);//creacion de la ruta de actualizacion
 
 module.exports = router;
 
