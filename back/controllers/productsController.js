f0ebd2e71e5adc43d662f7f180c19d7317f3a3d2
jhonreina <jonathang2812@gmail.com>
2022-10-21 11:59:36 -0500

    CRUD
     delete & fetch

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,5 +1,5 @@
-const productos = require("../models/productos");
 const producto = require("../models/productos");
+const fetch = (url) =>import('node-fetch').then(({default:fetch}) => fetch (url)); //Usurpacion del require
 // ver la lista de productos
 exports.getProducts = async (req, res, next) => {
     const productos = await producto.find();
@@ -35,7 +35,7 @@ exports.updateProduct = async (req, res, next) => {
       });
     }
     // si el objeto si existe ejecuto la actulizacion
-    product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+        product = await producto.findByIdAndUpdate(req.params.id, req.body, {
         new: true, //valida solo los atributos nuevos o actualizados
         runValidators: true,        
     });
@@ -55,3 +55,46 @@ exports.newProduct = async (req, res, next) => {
         product
     })    
 }
+
+
+//ELliminar producto
+
+exports.deleteProduct = async (req, res, next) => {
+    const product = await producto.findById(req.params.id);
+    if (!product) { //verifica que el objeto no existe
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos el producto",
+        });
+    }
+
+    await product.remove();//eliminamos el objeto
+    res.status(200).json({
+        success: true,
+        message:"Producto eliminado correctamente"
+    })
+}
+
+
+// HABLEMOS DE FETCH
+// ver todos los priductos
+function verProductos(params) {
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log (res))
+     .catch(err=>console.log(err))
+}
+
+// verProductos(); llamamos el metodo creado para probar consulta
+
+
+// ver por id
+
+function idProductos(id) {
+  fetch("http://localhost:4000/api/producto/"+id)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+}
+
+// idProductos("6351d035c0195484a7e91453"); probamos el metodo con id
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, newProduct, getProductById, updateProduct } = require("../controllers/productsController");
+const { getProducts, newProduct, getProductById, updateProduct, deleteProduct } = require("../controllers/productsController");
 
 //traemos la respuesta json desde el controlador
 //establecemos desde que ruta queremos ver el getproducts
@@ -8,6 +8,7 @@ router.route('/productos').get(getProducts)
 router.route('/producto/nuevo').post(newProduct)//Establecemos ruta
 router.route('/producto/:id').get(getProductById);//ruta para consultar ID 
 router.route('/producto/:id').put(updateProduct);//creacion de la ruta de actualizacion
+router.route('/producto/:id').delete(deleteProduct);//creacion de la ruta de eliminacion
 
 module.exports = router;
 
