b7244ffd4d7f2348ddf179a98580a3f20ad39c2c
jhonreina <jonathang2812@gmail.com>
2022-10-22 11:09:51 -0500

    creamos front

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>tienda virtual Queen</h1>
+        <img src='./images/logo2.png' alt='logo'></img>
+    </div>
+  );
+}
+
+export default App;
