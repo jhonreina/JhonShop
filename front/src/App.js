a8baf5c09f13cd710ef8b62a27d9487c903343df
jhonreina <jonathang2812@gmail.com>
2022-10-24 13:20:38 -0500

    Header & Footer

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-        <h1>tienda virtual Queen</h1>
-        <img src='./images/logo2.png' alt='logo'></img>
+      <Header />
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <Footer />
     </div>
   );
 }
---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+function Footer() {
+  return (
+      <Fragment>
+          <footer className='py-1'>
+              <p className='text-center mt-1'>
+                Queen Shop Store - 2022 - Jonathan Reina - Todos los derechos reservados.
+              </p>
+          </footer>
+     </Fragment>
+  )
+}
+
+export default Footer
